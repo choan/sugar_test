f27ff011d9ec732d633cae6711fb153f2e6372cd
Choan Galvez <choan.galvez@gmail.com>
2008-11-16 22:00:38 +0100

    Contexts can be now unnamed, the context aliaser is now a generator too and accepts a prefix

--- src/jshoulda.js
+++ src/jshoulda.js
@@ -24,40 +24,43 @@ var jShoulda = function() {
       }
     };
   }
-  
-  function context(name, config, args) {
-    var queue = Array.prototype.slice.call(arguments, 0);
-    var cName = '';
-    var obj = {};
-    // shift arguments if the first one is a string
-    if (typeof queue[0] == 'string') {
-      cName = queue.shift();
-    }
-    // shift arguments if the second one is a
-    // configuration object
-    if (typeof queue[0] === 'object') {
-      obj = queue.shift();
-    }
-    obj.before = obj.setup || obj.before || dummy;
-    obj.after = obj.teardown || obj.after || dummy;
-    var beforeQueue;
-    var afterQueue;
-    return function(outerName, before, after) {
-      // the root context gets no outerName or a configuration object
-      // as its first argument
-      var prefix = outerName;
-      var is_root = !!(outerName == undefined || typeof outerName == 'object');
-      if (is_root) {
-        tr = new Test.Unit.Runner({}, outerName || {});
-        prefix = '';
+  function getContextAlias(prefix) {
+    if (prefix) prefix += ' ';
+    return function (name, config, args) {
+      var queue = Array.prototype.slice.call(arguments, 0);
+      var cName = '';
+      var obj = {};
+      // shift arguments if the first one is a string
+      if (typeof queue[0] == 'string') {
+        if (name) { cName += prefix; }
+        cName += queue.shift();
       }
-      beforeQueue = before ? before.push(obj.before) && before : [obj.before];
-      afterQueue = after ? after.push(obj.after) && after : [obj.after];
-      runQueue(queue, [prefix, cName].join(' '), beforeQueue, afterQueue);
-      if (is_root) {
-        return tr;
+      // shift arguments if the second one is a
+      // configuration object
+      if (typeof queue[0] === 'object') {
+        obj = queue.shift();
       }
-      return false; // do not add to the tests queue
+      obj.before = obj.setup || obj.before || dummy;
+      obj.after = obj.teardown || obj.after || dummy;
+      var beforeQueue;
+      var afterQueue;
+      return function(outerName, before, after) {
+        // the root context gets no outerName or a configuration object
+        // as its first argument
+        var prefix = outerName;
+        var is_root = !!(outerName == undefined || typeof outerName == 'object');
+        if (is_root) {
+          tr = new Test.Unit.Runner({}, outerName || {});
+          prefix = '';
+        }
+        beforeQueue = before ? before.push(obj.before) && before : [obj.before];
+        afterQueue = after ? after.push(obj.after) && after : [obj.after];
+        runQueue(queue, [prefix, cName].join(' '), beforeQueue, afterQueue);
+        if (is_root) {
+          return tr;
+        }
+        return false; // do not add to the tests queue
+      };
     };
   };
 
@@ -84,20 +87,21 @@ var jShoulda = function() {
   }
   
   function setContextAlias(name, host) {
+    var prefix = '';
+    if (typeof host == 'string') {
+      prefix = host;
+      host = arguments[2];
+    }
     host = host || window;
-    host[name] = getContextAlias();
+    host[name] = getContextAlias(prefix);
     return jShoulda;
   }
   
-  function getContextAlias() {
-    return context;
-  }
-
 
   return {
     setShouldAlias : setShouldAlias,
-    setContextAlias : setContextAlias,
-  }
+    setContextAlias : setContextAlias
+  };
 
 }();
 
